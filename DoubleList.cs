cb8d4fb2d3307b867e64350dad652830e68b19f7
Stark <anfernoangel@gmail.com>
2020-12-01 20:44:33 +0200

    Добавьте файлы проекта.

---
+++ DoubleList.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyList
+{
+    class DoubleList<T> : IEnumerable<T>
+    {
+
+        protected class Node<TN>
+        {
+            public TN Data { get; set; }
+            public Node(TN data)
+            {
+                Data = data;
+            }
+            public Node<TN> Next { get; set; }
+            public Node<TN> Previous { get; set; }
+        }
+
+        private Node<T> _head;
+        private Node<T> _tail;
+        private int _count;
+        public int Count => _count;
+        public bool IsEmpty => _count == 0;
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+        /// <summary>
+        /// AddLast for initializer list
+        /// </summary>
+        /// <param name="data"></param>
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (_head == null)
+                _head = node;
+            else
+            {
+                _tail.Next = node;
+                node.Previous = _tail;
+            }
+
+            _tail = node;
+            _count++;
+        }
+        public void AddLast(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (_head == null)
+                _head = node;
+            else
+            {
+                _tail.Next = node;
+                node.Previous = _tail;
+            }
+
+            _tail = node;
+            _count++;
+        }
+
+        public void AddFirst(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            Node<T> temp = _head;
+            node.Next = temp;
+            _head = node;
+            if (_count == 0)
+                _tail = _head;
+            else
+                temp.Previous = node;
+            _count++;
+        }
+
+        public void Insert(T data, int pos)
+        {
+            if (pos < 0 || pos > _count)
+            {
+                return;
+            }
+            if (pos == 0)
+            {
+                AddFirst(data);
+                return;
+            }
+            if (pos == _count)
+            {
+                AddLast(data);
+                return;
+            }
+
+            var tmp = new Node<T>(data);
+            var cur = _head;
+            for (int i = 0; i < pos-1; i++)
+            {
+                cur.Data = cur.Data;
+                cur = cur.Next;
+            }
+
+            tmp.Previous = cur;
+            tmp.Next = cur.Next;
+            cur.Next.Previous = tmp;
+            cur.Next = tmp;
+            _count++;
+        }
+
+        public void PopFront()
+        {
+            if(_count==0)
+                return;
+            if (_count == 1)
+            {
+                _head = null;
+                _head = _tail = null;
+                _count = 0;
+                return;
+            }
+
+            _head = _head.Next;
+            _head.Previous = null;
+            _count--;
+        }
+
+        public void PopBack()
+        {
+            if (_count == 0)
+                return;
+            if (_count == 1)
+            {
+                _head = null;
+                _head = _tail = null;
+                _count = 0;
+                return;
+            }
+
+            _tail = _tail.Previous;
+            _tail.Next = null;
+            _count--;
+        }
+
+        //public bool Delete(T data)
+        //{
+        //    Node<T> current = new Node<T>(data);
+        //    while (current != null)
+        //    {
+        //        if (current.Data.Equals(data))
+        //            break;
+        //        current = current.Next;
+        //    }
+
+        //    if (current != null)
+        //    {
+        //        if (current.Next != null)
+        //            current.Next.Previous = current.Previous;
+        //        else
+        //            _tail = current.Previous;
+
+        //        if (current.Previous != null)
+        //            current.Previous.Next = current.Next;
+        //        else
+        //            _head = current.Next;
+
+        //        _count--;
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
+
+        public bool Contains(T data)
+        {
+            Node<T> current = _head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void PrintForward()
+        {
+            var cur = _head;
+            while (cur!=null)
+            {
+                Console.Write($"{cur.Data} ");
+                cur = cur.Next;
+            }
+
+            Console.WriteLine();
+        }
+
+        public void PrintBackward()
+        {
+            var cur = _tail;
+            while (cur != null)
+            {
+                Console.Write($"{cur.Data} ");
+                cur = cur.Previous;
+            }
+
+            Console.WriteLine();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = _head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerable<T> BackEnumerator()
+        {
+            Node<T> current = _tail;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Previous;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyList
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DoubleList<int> dl = new DoubleList<int>(){2,3,4,6,7,9,-2};
+            //dl.AddFirst(100);
+            //dl.AddLast(-100);
+            //dl.PrintForward();
+            //dl.PrintBackward();
+            DoubleList<int> doubleList = new DoubleList<int>() { 2,3,4 };
+            doubleList.PrintForward();
+            doubleList.Insert(5,0);
+            //doubleList.PopBack();
+            //doubleList.PopBack();
+            //doubleList.PopBack();
+            //dl1.PopFront();
+            //dl1.PopFront();
+            //dl1.PopFront();
+            doubleList.PrintForward();
+            doubleList.Insert(50,1);
+            doubleList.PrintForward();
+            doubleList.Insert(500,doubleList.Count);
+            doubleList.PrintForward();
+            doubleList.Insert(-999,3);
+            doubleList.PrintForward();
+            //Console.WriteLine(doubleList.Count);
+            doubleList.Clear();
+
+
+            //foreach (var i in dl)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(dl.Contains(50));
+            //Console.WriteLine(dl.Count);
+            //Console.WriteLine();
+            //dl.Delete(3);
+            //dl.Delete(2);
+            //foreach (var i in dl)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine(dl.Count);
+
+
+            //LinkedList<int> c = new LinkedList<int>();
+            //c.AddFirst(1);
+            //c.AddFirst(2);
+            //c.AddFirst(3);
+            //c.AddFirst(4);
+            //c.AddFirst(5);
+            
+            //c.Remove(2);
+            //foreach (var i in c)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+
+            //DoublyLinkedList<string> linkedList = new DoublyLinkedList<string>();
+            //// добавление элементов
+            //linkedList.AddLast("Bob");
+            //linkedList.AddLast("Bill");
+            //linkedList.AddLast("Tom");
+            //linkedList.AddFirst("Kate");
+            //foreach (var item in linkedList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine();
+            //// удаление
+            //linkedList.Remove("Bill");
+
+            //// перебор с последнего элемента
+            //foreach (var t in linkedList.BackEnumerator())
+            //{
+            //    Console.WriteLine(t);
+            //}
+        }
+    }
+}
